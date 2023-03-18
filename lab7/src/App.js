70d9d9622e9bebfc6921776463b6a8038bc1f01f
KeonDawoudi <47834137+KeonDawoudi@users.noreply.github.com>
2023-03-18 13:53:27 -0400

    react lab template code

---
+++ lab7/src/App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+
+
+<nav class="navbar navbar-light bg-primary">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1 text-white">Password Genie</span>
+  </div>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3 px-1 bg-dark position-fixed" id="sticky-sidebar">
+            ...
+            <div class="text-white">item</div>
+            <div class="text-white">item</div>
+            <div class="text-white">item</div>
+        </div>
+        <div class="col offset-3" id="main">
+            <h1>Generate password</h1>
+            ...
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container my-5">
+
+  <footer class="text-center text-lg-start position-absolute bottom-0 left-50" >
+    <div class="container d-flex justify-content-center py-5">
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
+        <h1>a</h1>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
+      <h1>a</h1>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
+      <h1>a</h1>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
+      <h1>a</h1>
+      </button>
+    </div>
+
+    <div class="text-center text-white p-3">
+      © 2023 Copyright:
+     
+    </div>
+ 
+  </footer>
+  
+</div>
+    </div>
+  );
+}
+
+export default App;
