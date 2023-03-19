ddcfe2323914fce28143ff9810264cf372dbd3d5
Tag <tgaud061@uottawa.ca>
2023-03-19 11:15:57 -0400

    Changes

--- lab8/src/App.js
+++ lab8/src/App.js
@@ -1,62 +1,41 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/js/bootstrap.js';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-
-
-
-
-<nav class="navbar navbar-light bg-primary">
-  <div class="container-fluid">
-    <span class="navbar-brand mb-0 h1 text-white">Password Genie</span>
-  </div>
-</nav>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-3 px-1 bg-dark position-fixed" id="sticky-sidebar">
-            ...
-            <div class="text-white">item</div>
-            <div class="text-white">item</div>
-            <div class="text-white">item</div>
+      <nav class="navbar navbar-light bg-primary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1 text-white">Password Genie</span>
         </div>
-        <div class="col offset-3" id="main">
-            <h1>Generate password</h1>
-            ...
-        </div>
-    </div>
-</div>
+      </nav>
+      <div class="container-fluid">
+          <div class="row">
+            <nav class="col-2 border border-primary rounded m-3">
+              <ul class="nav flex-column">
+                <li class="m-3"><img src="..." alt="Characters" class="img-thumbnail"/></li>
+                <li class="m-3"><img src="..." alt="Symbols" class="img-thumbnail"/></li>
+                <li class="m-3"><img src="..." alt="Numbers" class="img-thumbnail"/></li>
+              </ul>
+            </nav>
+            <div class="col-10 offset-2" id="main">
+              <h1>Generate password</h1>
+              <p>
+                Your Password (Click to copy)
+              </p>
+              <input
+                type='range'
+                class="form-range"
+                min={4}
+                max={128}
+                step={1}
+                className='custom-slider'/>
 
-
-
-<div class="container my-5">
-
-  <footer class="text-center text-lg-start position-absolute bottom-0 left-50" >
-    <div class="container d-flex justify-content-center py-5">
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
-        <h1>a</h1>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
-      <h1>a</h1>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
-      <h1>a</h1>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" >
-      <h1>a</h1>
-      </button>
-    </div>
-
-    <div class="text-center text-white p-3">
-      © 2023 Copyright:
-     
-    </div>
- 
-  </footer>
-  
-</div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
