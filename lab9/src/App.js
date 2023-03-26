3e2b1d86a3140c850a344f26f1ff2246297305d4
Tag <tgaud061@uottawa.ca>
2023-03-26 18:06:39 -0400

    Fixed hover

--- lab9/src/App.js
+++ lab9/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import 'bootstrap/dist/js/bootstrap.js';
-import './App.css';
-import GreyableImage from './Criteria';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "bootstrap/dist/js/bootstrap.js";
+import "./App.css";
+import GreyableImage from "./Criteria";
+import { useState } from "react";
 import { motion } from "framer-motion";
-
 
 function App() {
   const text = {
@@ -29,7 +28,7 @@ function App() {
       copied: "Copié!",
       copy: "Cliquer pour Copier",
       length: "Longueur",
-    }
+    },
   };
 
   const [length, setLength] = useState(32);
@@ -46,41 +45,43 @@ function App() {
     setLength(event.target.value);
   }
 
-  function generatePassword(){
-    let characterSet = '';
-    let result = '';
+  function generatePassword() {
+    let characterSet = "";
+    let result = "";
 
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    const numbers = '0123456789';
-    const symbols = '!@#$%^&*()-_+';
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()-_+";
 
     if (useCharacters) {
       characterSet += characters;
     }
     if (useNumbers) {
-      characterSet += numbers; 
+      characterSet += numbers;
     }
     if (useSymbols) {
       characterSet += symbols;
     }
 
     const characterSetLength = characterSet.length;
-    
+
     for (let i = 0; i < length; i++) {
-      result += characterSet.charAt(Math.floor(Math.random() * characterSetLength));
+      result += characterSet.charAt(
+        Math.floor(Math.random() * characterSetLength)
+      );
     }
-    
+
     setPassword(result);
   }
 
-  function copyPassword(){
+  function copyPassword() {
     navigator.clipboard.writeText(password);
     setCopied(true);
     setTimeout(() => setCopied(false), 1500);
   }
 
-  function toggleLanguage(){
-    setLanguage(language=="en"?"fr":'en');
+  function toggleLanguage() {
+    setLanguage(language == "en" ? "fr" : "en");
   }
 
   return (
@@ -88,70 +89,83 @@ function App() {
       <nav class="navbar navbar-light bg-primary">
         <div class="container-fluid">
           <h1 class="bg-primary text-white m-3">Password Genie</h1>
-          <h3 class="bg-primary text-white m-3" onClick={toggleLanguage}>{language}</h3>
+          <h3 class="bg-primary text-white m-3" onClick={toggleLanguage}>
+            {language == "en" ? "fr" : "en"}
+          </h3>
         </div>
       </nav>
       <div class="container-fluid">
-    <div class="row">
-      <div class="col-2 border border-primary rounded m-3">
-        <ul class="nav flex-column">
-          <li class="m-3">
-            <GreyableImage
-              src="characters.png"
-              alt={text[language].characters}
-              title={text[language].characters}
-              active={useCharacters}
-              setActive={setUseCharacters}
+        <div class="row">
+          <div class="col-2 border border-primary rounded m-3">
+            <ul class="nav flex-column">
+              <li class="m-3">
+                <GreyableImage
+                  src="characters.png"
+                  alt={text[language].characters}
+                  title={text[language].characters}
+                  active={useCharacters}
+                  setActive={setUseCharacters}
+                />
+                <h5 class="text-primary">{text[language].characters}</h5>
+              </li>
+              <li class="m-3">
+                <GreyableImage
+                  src="symbols.png"
+                  alt={text[language].symbols}
+                  class="img-thumbnail"
+                  title={text[language].symbols}
+                  active={useSymbols}
+                  setActive={setUseSymbols}
+                />
+                <h5 class="text-primary">{text[language].symbols}</h5>
+              </li>
+              <li class="m-3">
+                <GreyableImage
+                  src="twofactor.png"
+                  alt={text[language].numbers}
+                  class="img-thumbnail"
+                  title={text[language].numbers}
+                  active={useNumbers}
+                  setActive={setUseNumbers}
+                />
+                <h5 class="text-primary">{text[language].numbers}</h5>
+              </li>
+            </ul>
+          </div>
+          <div class="col m-3">
+            <motion.div whileHover={{ scale: 1.05 }}>
+              <h1
+                class="border border-primary text-primary p-3 rounded"
+                onClick={generatePassword}
+              >
+                {text[language].generate}
+              </h1>
+            </motion.div>
+
+            <motion.div whileHover={{ scale: 1.05 }}>
+              <h5
+                whileHover={{ scale: 1.05 }}
+                class="border border-secondary text-secondary p-3 rounded"
+                onClick={copyPassword}
+              >
+                {text[language].yourPassword} {password} (
+                {copied ? text[language].copied : text[language].copy})
+              </h5>
+            </motion.div>
+            <h6 class="text-primary mt-3">{text[language].length}</h6>
+            <input
+              type="range"
+              class="form-range"
+              min={4}
+              max={128}
+              step={1}
+              onChange={sliderChange}
             />
-            <h5 class="text-primary">{text[language].characters}</h5>
-          </li>
-          <li class="m-3">
-            <GreyableImage
-              src="symbols.png"
-              alt={text[language].symbols}
-              class="img-thumbnail"
-              title={text[language].symbols}
-              active={useSymbols}
-              setActive={setUseSymbols}
-            />
-            <h5 class="text-primary">{text[language].symbols}</h5>
-          </li>
-          <li class="m-3">
-            <GreyableImage
-              src="twofactor.png"
-              alt={text[language].numbers}
-              class="img-thumbnail"
-              title={text[language].numbers}
-              active={useNumbers}
-              setActive={setUseNumbers}
-            />
-            <h5 class="text-primary">{text[language].numbers}</h5>
-          </li>
-        </ul>
-      </div>
-      <div class="col m-3">
-        <motion.div
-          whileHover={{ scale: 1.2 }}
-          onHoverStart={e => {}}
-          onHoverEnd={e => {}}
-          class="border border-primary text-primary p-3 rounded"
-          onClick={generatePassword}
-        >
-          {text[language].generate}
-        </motion.div>
-        <h5
-          class="border border-secondary text-secondary p-3 rounded"
-          onClick={copyPassword}
-        >
-          {text[language].yourPassword} {password} ({copied ? text[language].copied : text[language].copy})
-        </h5>
-        <h6 class="text-primary mt-3">{text[language].length}</h6>
-        <input type="range" class="form-range" min={4} max={128} step={1} onChange={sliderChange} />
-        <h6 class="text-primary">{length}</h6>
+            <h6 class="text-primary">{length}</h6>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
   );
 }
 
